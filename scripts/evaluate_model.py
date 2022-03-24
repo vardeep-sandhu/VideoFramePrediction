36017560cb429733ace6fdd088c6a422da256796
here-to-learn0 <good.coding.is.that.you@gmail.com>
2022-03-24 22:15:12 +0100

    better eval

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 from model import Model
@@ -6,7 +7,6 @@ import argparse
 from dataset.test_moving_mnist import MovingMNIST_test
 from dataset.kth import KTH
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 import piqa
 from skimage.metrics import structural_similarity 
@@ -14,6 +14,9 @@ from skimage.metrics import structural_similarity
 def parse_commandline():
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_dataset', '-d', type=str)
+    parser.add_argument('--model_path', '-mp', type=str)
+    parser.add_argument('--save_path', '-s', type=str)
+
     args = parser.parse_args()
     # cfg = AttrDict(utils.load_cfg(args.cfg_name))
     return args
@@ -39,34 +42,34 @@ def ssim_metric(x1, x2):
     return ssim
 
 def evaluate_metrices(prediction, target):
-    batch_size, seq_len = target.shape[0:2] 
-    mae = torch.zeros((batch_size, seq_len))
-    mse = torch.zeros((batch_size, seq_len))
-    psnr = torch.zeros((batch_size, seq_len))
-    lpips = torch.zeros((batch_size, seq_len))
-    ssim = torch.zeros((batch_size, seq_len))
 
-    for b_inx in range(batch_size):
+    psnr_seq = piqa.psnr.psnr(prediction[0], target[0])
+    lpips_criterion = piqa.lpips.LPIPS().cuda()
+    mse_seq, mae_seq, lpips_seq = all_metric(lpips_criterion, prediction[0], target[0])
+    ssim_seq = ssim_metric(prediction[0].cpu().numpy(), target[0].cpu().numpy())
 
-        psnr_seq = piqa.psnr.psnr(prediction[b_inx], target[b_inx])
-        lpips_criterion = piqa.lpips.LPIPS().cuda()
-        mse_seq, mae_seq, lpips_seq = all_metric(lpips_criterion, prediction[b_inx], target[b_inx])
-        # ssim_seq = ssim_metric(prediction[b_inx].cpu().numpy(), target[b_inx].cpu().numpy())
+    return mae_seq, mse_seq, psnr_seq, lpips_seq, ssim_seq
 
-        mae[b_inx, :] = mae_seq
-        mse[b_inx, :] = mse_seq
-        psnr[b_inx, :] = psnr_seq
-        lpips[b_inx, :] = lpips_seq
-        # ssim[b_inx, :] = ssim_seq
+def save_tensors(stats, save_dir ):
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    for matrix in stats:
+        torch.save(stats[matrix], f'{matrix}.pt')
+
+def print_results(stats):
+    print("*" * 50)
     
-    return mae, mse, psnr, lpips, ssim
-    
+    for matrix in stats:
+        print(f"Overall {matrix}")
+        print(torch.mean(stats[matrix]).item())
+        print(torch.mean(stats[matrix], dim=0))
+        print("*" * 50)
 
 
 def main():
     args = parse_commandline()
     device = "cuda"
-    model_path = "models/model_3.pth"
+    model_path = args.model_path
     model = Model()
     model, _, _ = loading_model(model, model_path)
     model = model.to(device)
@@ -76,39 +79,38 @@ def main():
                     ])
 
     if args.test_dataset == "moving_mnist":
-        test_set = MovingMNIST_test(root='data/mnist', train=False, download=True, transform=transform, target_transform=transform, split=100)
+        test_set = MovingMNIST_test(root='data/mnist', train=False, download=True, transform=transform, target_transform=transform, split=10_000)
+    
     if args.test_dataset == "kth":
         test_set = KTH("data/kth", transform=transform, download=False, train=False)
     print(len(test_set))
 
-    test_loader = DataLoader(dataset=test_set, batch_size=32, shuffle=False)
-
-    mae_all = []
-    mse_all = []
-    psnr_all = []
-    lpips_all = []
-    ssim_all = []
+    mae = torch.zeros((len(test_set), 10))
+    mse = torch.zeros((len(test_set), 10))
+    psnr = torch.zeros((len(test_set), 10))
+    lpips = torch.zeros((len(test_set), 10))
+    ssim = torch.zeros((len(test_set), 10))
 
     with torch.no_grad():
         
-        pbar = tqdm(enumerate(test_loader), total=len(test_loader))
-        for _, (seq, target) in pbar:
+        pbar = tqdm(enumerate(test_set), total=len(test_set))
+        for b_inx, (seq, target) in pbar:
+            seq, target = seq.unsqueeze(0), target.unsqueeze(0)
             seq = seq.type(torch.FloatTensor).to(device)
             target = target.type(torch.FloatTensor).to(device)
             
             predictions = model(seq)
             predictions = predictions.to(device)
                     
-            mae, mse, psnr, lpips, ssim  = evaluate_metrices(predictions[:, 10:, :, :, :], target)
-            mae_all.append(mae)
-            mse_all.append(mse)
-            psnr_all.append(psnr)
-            lpips_all.append(lpips)
-            ssim_all.append(ssim)
-
-            # pbar.set_description(f"Test loss: loss {loss.item():.2f}")
-        mae_all, mse_all, psnr_all, lpips_all, ssim_all = torch.cat(mae_all), torch.cat(mse_all), torch.cat(psnr_all), torch.cat(lpips_all), torch.cat(ssim_all) 
-        return mae_all, mse_all, psnr_all, lpips_all, ssim_all 
+            mae_seq, mse_seq, psnr_seq, lpips_seq, ssim_seq  = evaluate_metrices(predictions[:, 10:, :, :, :], target)
+            mae[b_inx, :] = mae_seq
+            mse[b_inx, :] = mse_seq
+            psnr[b_inx, :] = psnr_seq
+            lpips[b_inx, :] = lpips_seq
+            ssim[b_inx, :] = ssim_seq
+        stats = {"MAE": mae, "MSE": mse,"PSNR": psnr,"LPIPS": lpips,"SSIM": ssim}
+        save_tensors(stats)
+        print_results(stats)
 
 
 if __name__ == "__main__":
